d5b41912f0a653b5d819fe362bbff9f7276d698e
Michele Dexter <micheleannedexter@gmail.com>
2018-04-18 10:20:48 -0500

    change to ES6

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
+// Get server set up
 const express = require('express');
-
 const app = express();
-const quotes = require('./all-quotes.js');
 const PORT = 5000;
 
-app.get('/', function (req, res) {
-    res.send('Happy Wednesday!');
-});
+// Get additional requirements
+const quotes = require('./all-quotes.js');
 
-app.get('/all-quotes', function (req, res) {
+// Start express server to listen on port 5000
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+// Set root page response
+app.get('/', (req, res) => res.send('Happy Wednesday!'));
+
+// Set all-quotes subdirectory response
+app.get('/all-quotes', (req, res) => {
     let response = '';
     quotes.forEach((data) => response += `<p>"${data.quote}" - ${data.author}</p>`);
     res.send(response);
-});
-
-app.listen(PORT, function () {
-    console.log(`Listening on port ${PORT}`);
 });
