503efb457de63835c71ec807d9ed67d8e1b82b8c
Michele Dexter <micheleannedexter@gmail.com>
2018-04-18 10:46:55 -0500

    add index and debugger

--- server/server.js
+++ server/server.js
@@ -6,15 +6,24 @@ const PORT = 5000;
 // Get additional requirements
 const quotes = require('./all-quotes.js');
 
-// Start express server to listen on port 5000
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
 // Set root page response
-app.get('/', (req, res) => res.send('Happy Wednesday!'));
+app.get('/', (req, res) => {
+    loaded();
+    res.send('Happy Wednesday!');
+});
 
 // Set all-quotes subdirectory response
 app.get('/all-quotes', (req, res) => {
+    loaded();
     let response = '';
     quotes.forEach((data) => response += `<p>"${data.quote}" - ${data.author}</p>`);
     res.send(response);
 });
+
+// Start express server to listen on port 5000
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+// Prepare page load message
+function loaded() {
+    console.log("Page loaded");
+}
