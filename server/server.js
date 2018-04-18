9462284a57e111c6b0a19349b1fed4e81f260fdd
Michele Dexter <micheleannedexter@gmail.com>
2018-04-18 10:14:07 -0500

    add all-quotes module

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
 const app = express();
+const quotes = require('./all-quotes.js');
 const PORT = 5000;
 
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
     res.send('Happy Wednesday!');
+});
+
+app.get('/all-quotes', function (req, res) {
+    let response = '';
+    quotes.forEach((data) => response += `<p>"${data.quote}" - ${data.author}</p>`);
+    res.send(response);
 });
 
 app.listen(PORT, function () {
